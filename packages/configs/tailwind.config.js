4f3a634a0aae6c0295983cae79c52b0b2fe59227
vivekpal1 <vivekpal@duck.com>
2023-10-08 01:50:10 +0530

    theme

--- packages/configs/tailwind.config.js
+++ packages/configs/tailwind.config.js
@@ -5,7 +5,29 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        '6rem': '6rem',
+      },
+      borderRadius: {
+        custom: 'var(--border-radius)',
+      },
+      maxWidth: {
+        custom: 'var(--max-width)',
+      },
+      colors: {
+        callout: {
+          DEFAULT: 'var(--callout-rgb)',
+          border: 'var(--callout-border-rgb)',
+        },
+        card: {
+          DEFAULT: 'var(--card-rgb)',
+          border: 'var(--card-border-rgb)',
+        },
+        backgroundStart: 'var(--background-start-rgb)',
+        backgroundEnd: 'var(--background-end-rgb)',
+      },
+    },
   },
   plugins: [],
 };
