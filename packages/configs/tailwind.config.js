fea799e6c55e2375e68b81fb72bc49b312613035
vivekpal1 <vivekpal@duck.com>
2023-10-08 03:39:59 +0530

    button

---
+++ packages/configs/tailwind.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  content: [
+    "../../packages/ui-components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      spacing: {
+        '6rem': '6rem',
+      },
+      borderRadius: {
+        custom: 'var(--border-radius)',
+      },
+      maxWidth: {
+        custom: 'var(--max-width)',
+      },
+      colors: {
+        callout: {
+          DEFAULT: 'var(--callout-rgb)',
+          border: 'var(--callout-border-rgb)',
+        },
+        card: {
+          DEFAULT: 'var(--card-rgb)',
+          border: 'var(--card-border-rgb)',
+        },
+        backgroundStart: 'var(--background-start-rgb)',
+        backgroundEnd: 'var(--background-end-rgb)',
+      },
+    },
+  },
+  plugins: [],
+};
--- packages/ui-components/components/Button.tsx
+++ packages/ui-components/components/Button.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../styles.css';
 
 interface ButtonProps {
   variant?: 'primary' | 'secondary';
@@ -6,9 +7,9 @@ interface ButtonProps {
 }
 
 export const Button: React.FC<ButtonProps> = ({ variant = 'primary', children }) => {
-  const baseClasses = "py-2 px-4 rounded";
-  const primaryClasses = "bg-blue-500 text-white hover:bg-blue-600";
-  const secondaryClasses = "bg-gray-300 text-black hover:bg-gray-400";
+  const baseClasses = "py-2 px-4 rounded border border-gray-300 hover:border-gray-500 text-gray-600 hover:text-black font-mono text-sm transition-all duration-300";
+  const primaryClasses = "bg-blue-500 text-white hover:bg-blue-600 border-blue-500 hover:border-blue-600";
+  const secondaryClasses = "bg-transparent";
 
   const finalClasses = `
     ${baseClasses} 
@@ -17,3 +18,5 @@ export const Button: React.FC<ButtonProps> = ({ variant = 'primary', children })
 
   return <button className={finalClasses}>{children}</button>;
 };
+
+export default Button;
