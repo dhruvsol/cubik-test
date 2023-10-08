5f40752b45cdb880c0d1d45ee18531b851ec0cf6
Sayam Samal <samal.sayam@gmail.com>
2023-10-09 05:05:01 +0530

    web-app: Rewrite page.tsx using Tailwind.

--- packages/tailwind-config/tailwind.config.ts
+++ packages/tailwind-config/tailwind.config.ts
@@ -8,7 +8,12 @@ const config: Config = {
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "glow-conic":
+          "conic-gradient(from 180deg at 50% 50%, #2a8af6 0deg, #a853ba 180deg, #e92a67 360deg)",
+      },
+    },
   },
   plugins: [],
 };
