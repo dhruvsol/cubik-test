6b2fa2025165cca36b15e90fc52c462c2f9c25bd
Sayan Biswas <me@sayanbiswas.in>
2023-10-08 23:10:48 +0530

    Fix build issues and remove redundant code
    Signed-off-by: Sayan Biswas <me@sayanbiswas.in>

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,5 +1,11 @@
+import { Button } from "@ui/components/button";
+
 export default function Page(): JSX.Element {
   return (
-    <h1 className="text-3xl">Hello</h1>
+    <>
+    <h1 className="text-3xl">Hello </h1>
+    <Button variant="outline">Button 1</Button>
+    <Button variant="secondary">Button 2</Button>
+    </>
   );
 }
